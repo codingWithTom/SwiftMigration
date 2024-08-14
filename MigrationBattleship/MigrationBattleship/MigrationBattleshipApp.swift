1b9920078567dc4b027073207a8e633f9945c7c4
Coding with Tom <codingwithtom138@gmail.com>
2024-08-14 15:53:15 -0700

    Project setup

---
+++ MigrationBattleship/MigrationBattleship/MigrationBattleshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigrationBattleshipApp.swift
+//  MigrationBattleship
+//
+//  Created by Tomas Trujillo on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct MigrationBattleshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
